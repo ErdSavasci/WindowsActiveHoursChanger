0f868b418bd4e9fa58a6a014142a0582dec6c950
Erdem Savasci <savasci.erd@gmail.com>
2023-10-14 20:49:37 +0300

    Some fixes are done on the task generation script.

--- CreateAutomatedTask_Windows10ActiveHoursChanger.cmd
+++ CreateAutomatedTask_Windows10ActiveHoursChanger.cmd
@@ -1,6 +1,8 @@
 @ECHO OFF
 
-schtasks /create /sc ONLOGON /tn "UAC\Windows 10 Active Hours Changer" /tr "%cd%\Releases\v1.2\Windows10ActiveHoursChanger.exe" /rl HIGHEST /it /f
+set "current_script_path=%~dp0"
+
+schtasks /create /sc ONLOGON /tn "UAC\Windows 10 Active Hours Changer" /tr "\"%current_script_path%\Releases\v1.2\Windows10ActiveHoursChanger.exe\"" /rl HIGHEST /it /f
 schtasks /change /ri 60 /tn "UAC\Windows 10 Active Hours Changer"
 
 ECHO "Successfully added the task into the Task Scheduler.. Quitting.."
